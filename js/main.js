c1ac76254e3fe98c504c23c46cb09fb172b404d5
Alex <lexashkel@mal.ru>
2019-09-11 17:28:45 +0300

    1 задание - добавил переключение экранов

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
 (function () {
 
-  const greeting = document.getElementById(`greeting`).content.querySelector(`.greeting.central--blur`);
-  if (greeting) {
-    console.log(greeting);
-  }
-
-  const rules = document.getElementById(`rules`);
-  const rulesHeader = rules.content.querySelector(`.header`);
-  const rulesMain = rules.content.querySelector(`.rules`);
-  const rulesFooter = rules.content.querySelector(`.footer`);
-
+  const greeting = document.getElementById(`greeting`).content;
+  const rules = document.getElementById(`rules`).content;
   const game1 = document.getElementById(`game-1`).content;
+  const game2 = document.getElementById(`game-2`).content;
+  const game3 = document.getElementById(`game-3`).content;
+  const stats = document.getElementById(`stats`).content;
+
+  const screens = [greeting, rules, game1, game2, game3, stats];
+
+  const RIGHT_ARROW_KEYCODE = 39;
+
+  const centralScreen = document.querySelector(`.central`);
+
+  const showScreen = (screenNumber) => {
+    while (centralScreen.firstChild) {
+      centralScreen.removeChild(centralScreen.firstChild);
+    }
+    centralScreen.appendChild(screens[screenNumber]);
+
+  };
+
+  let currentScreen = 1;
+  showScreen(currentScreen);
+
+  document.addEventListener(`keydown`, function (evt) {
+    if (evt.keyCode === RIGHT_ARROW_KEYCODE) {
+      evt.preventDefault();
+      if (currentScreen < screens.length - 1) {
+        const elem = ++currentScreen;
+        showScreen(elem);
+      }
+    }
+  });
+
+
 })();
