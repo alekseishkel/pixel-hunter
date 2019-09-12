17d842b24892e553e43c0ee3d59459e627465f15
Alex <lexashkel@mal.ru>
2019-09-12 17:15:16 +0300

    №1 не доделал переключение на стрелки

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@
   const screens = [greeting, rules, game1, game2, game3, stats];
 
   const RIGHT_ARROW_KEYCODE = 39;
+  const LEFT_ARROW_KEYCODE = 37;
 
   const centralScreen = document.querySelector(`.central`);
 
@@ -17,19 +18,59 @@
     while (centralScreen.firstChild) {
       centralScreen.removeChild(centralScreen.firstChild);
     }
-    centralScreen.appendChild(screens[screenNumber]);
-
+    centralScreen.appendChild(screens[screenNumber].cloneNode(true));
   };
 
-  let currentScreen = 1;
+  let currentScreen = 3;
   showScreen(currentScreen);
+
+  document.body.insertAdjacentHTML(`beforeend`, `<div class="arrows__wrap">
+  <style>
+  .arrows__wrap {
+    position: absolute;
+    top: 95px;
+    left: 50%;
+    margin-left: -56px;
+  }
+  .arrows__btn {
+    background: none;
+    border: 2px solid black;
+    padding: 5px 20px;
+  }
+  </style>
+    <button class="arrows__btn"><-</button>
+    <button class="arrows__btn">-></button>
+  </div>`);
+
+  const arrows = document.querySelector(`.arrows__btn`);
+  console.log(arrows);
+  arrows.addEventListener(`DOMContentLoaded`, function (evt) {
+    // if (evt.keyCode === RIGHT_ARROW_KEYCODE) {
+    console.log(`hahaha`);
+    evt.preventDefault();
+    if (currentScreen < screens.length - 1) {
+      ++currentScreen;
+      showScreen(currentScreen);
+    }
+    // }
+  });
 
   document.addEventListener(`keydown`, function (evt) {
     if (evt.keyCode === RIGHT_ARROW_KEYCODE) {
       evt.preventDefault();
       if (currentScreen < screens.length - 1) {
-        const elem = ++currentScreen;
-        showScreen(elem);
+        ++currentScreen;
+        showScreen(currentScreen);
+      }
+    }
+  });
+
+  document.addEventListener(`keydown`, function (evt) {
+    if (evt.keyCode === LEFT_ARROW_KEYCODE) {
+      evt.preventDefault();
+      if (currentScreen > 0) {
+        --currentScreen;
+        showScreen(currentScreen);
       }
     }
   });
