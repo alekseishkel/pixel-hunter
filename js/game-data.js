ec6966a124dba76aa6679e596aa56b7fc6639d65
Alex <lexashkel@mal.ru>
2019-10-14 17:08:28 +0300

    #3 почти фулл

--- js/game-data.js
+++ js/game-data.js
@@ -1,8 +1,45 @@
 const INITIAL_GAME = Object.freeze({
   level: 0,
-  lives: 2,
+  lives: 3,
   time: 0
 });
+
+const answers = [
+  {
+    answer: true,
+    time: 6000
+  },
+  {
+    answer: true,
+    time: 3000
+  },
+  {
+    answer: true,
+    time: 16000
+  },
+  {
+    answer: true,
+    time: 1000
+  },
+  {
+    answer: true,
+    time: 9000
+  },
+];
+
+const answersNumber = 10;
+const answerSpeed = 3000;
+const lives = 3;
+const score = 1150;
+
+const checkAnswerSpeed = (speed) => {
+  if (speed < 5000) {
+    throw new Error(`Speed should be more then 5000ms`);
+  }
+  return score;
+};
+
+console.log(checkAnswerSpeed(10000));
 
 const changeLevel = (game, level) => {
   if (level < 0) {
@@ -19,5 +56,12 @@ const changeLevel = (game, level) => {
   return newGame;
 };
 
-module.exports.INITIAL_GAME = INITIAL_GAME;
-module.exports.changeLevel = changeLevel;
+module.exports = {
+  changeLevel,
+  INITIAL_GAME,
+  answersNumber,
+  answerSpeed,
+  lives,
+  checkAnswerSpeed
+};
+
--- js/test/game-data.test.js
+++ js/test/game-data.test.js
@@ -1,6 +1,6 @@
 
 const assert = require(`chai`).assert;
-const {changeLevel, INITIAL_GAME} = require(`../game-data`);
+const {changeLevel, INITIAL_GAME, answersNumber, answerSpeed, lives, checkAnswerSpeed} = require(`../game-data`);
 
 describe(`Check level changer`, () => {
   it(`should update level of the game`, () => {
@@ -17,5 +17,19 @@ describe(`Check level changer`, () => {
     assert.throws(() =>changeLevel(INITIAL_GAME, []).level, /Level should be of type number/);
     assert.throws(() =>changeLevel(INITIAL_GAME, null).level, /Level should be of type number/);
     assert.throws(() =>changeLevel(INITIAL_GAME, undefined).level, /Level should be of type number/);
+  });
+});
+
+describe(`Check points function`, () => {
+  it(`check that the user answered 10 questions`, () => {
+    assert.equal(answersNumber, 10);
+  });
+  it(`check answers speed and lives`, () => {
+    assert.throws(() =>checkAnswerSpeed(answerSpeed), /Speed should be more then 5000ms/);
+    // console.log(checkAnswerSpeed);
+    // console.log(answerSpeed);
+
+    // console.log(checkAnswerSpeed(answerSpeed));
+    assert.equal(lives, 3);
   });
 });
